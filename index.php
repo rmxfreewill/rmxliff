52f8b0de992af1d120a3e7ed6f9d1fab8ac4da31
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-10-11 22:27:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (isset($_POST['Function']))
     $Function = $_POST['Function'];
 if (isset($_GET['Function']))
     $Function = $_GET['Function'];
-
+echo $Function;
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
