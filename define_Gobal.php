c0d938a2cfef6ea54b71a76eae9be892221678d2
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-10-12 00:49:52 +0700

    Update define_Gobal.php

--- define_Gobal.php
+++ define_Gobal.php
@@ -1,5 +1,5 @@
 <?php
-define("LIFF_ID","1656503744-kojgw9pb");
+// define("LIFF_ID","1656503744-kojgw9pb");
 // define("sURL","https://rmxliff.ddns.net/");
 define("REGISTER_URL", "http://rmxcell.pe.hu/rmxLineRegister.php");
 define("COMPANY_URL", "http://rmxcell.pe.hu/rmxLineCmd.php");
@@ -8,5 +8,5 @@ define("COMPANY_CODE", "00001");
 
 
 
-// define("LIFF_ID", "1656520973-EzB8pRze");
+define("LIFF_ID", "1656520973-EzB8pRze");
 define("sURL", "https://rmxlineliff.herokuapp.com/");
