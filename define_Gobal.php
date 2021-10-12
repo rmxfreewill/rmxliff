f23e3efb869a8756ed0b38cdeab9da6fdb82b57c
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-10-13 00:58:04 +0700

    Update define_Gobal.php

--- define_Gobal.php
+++ define_Gobal.php
@@ -2,20 +2,20 @@
 
 //RMX Server
 define("COMPANY_CODE", "00001");
-define("LIFF_ID","1656503744-kojgw9pb");
-define("sURL","https://rmxliff.ddns.net/");
+// define("LIFF_ID","1656503744-kojgw9pb");
+// define("sURL","https://rmxliff.ddns.net/");
 define("COMPANY_URL", "http://119.46.97.156/rmxline/rmxLineCmd.php");
 define("REGISTER_URL", "http://119.46.97.156/rmxline/rmxLineRegister.php");
-define("RICHMENU_ID","richmenu-58ef9edf18ba477f9ea2a0f12c28794c");
-define("BEARER_TOKEN","6DOzScAqBRwD/oRPwvMFua/SBvgLtXciCay4cwK10oTPA88R60mjeGdeW8NDL61dCJX2EtyHINFcj1DvY0mboZntH38a/fhTRI3rCaN4vDI/zWBCl0ze5K/AV2JoxoCwR9OZXj2Y7rHn6nABPwZMVwdB04t89/1O/w1cDnyilFU=");
+// define("RICHMENU_ID","richmenu-58ef9edf18ba477f9ea2a0f12c28794c");
+// define("BEARER_TOKEN","6DOzScAqBRwD/oRPwvMFua/SBvgLtXciCay4cwK10oTPA88R60mjeGdeW8NDL61dCJX2EtyHINFcj1DvY0mboZntH38a/fhTRI3rCaN4vDI/zWBCl0ze5K/AV2JoxoCwR9OZXj2Y7rHn6nABPwZMVwdB04t89/1O/w1cDnyilFU=");
 
 //Heroku Server
-// define("LIFF_ID", "1656520973-EzB8pRze");
-// define("sURL", "https://rmxlineliff.herokuapp.com/");
+define("LIFF_ID", "1656520973-EzB8pRze");
+define("sURL", "https://rmxlineliff.herokuapp.com/");
 // define("COMPANY_URL", "http://rmxlineliff.herokuapp.com/zApi/rmxLineCmd.php");
 // define("REGISTER_URL", "http://rmxlineliff.herokuapp.com/zApi/rmxLineRegister.php");
-// define("RICHMENU_ID","richmenu-119fefe49b2dd01369a9416da62d7f80");
-// define("BEARER_TOKEN","EDiLRqCWwuFXTmT2KGXddtlV2GVSg9kaTWJuJvsonJ1bbAKPCKISIyhavW4D5tL5tY7L+sU8jUkh+V7bxIP6lLTo7aXpV+QTKthC3vXAho+2nq50e2ZrzJguKtoC6Nhp4CLJajUtheyDbCyHvcHQ/gdB04t89/1O/w1cDnyilFU=");
+define("RICHMENU_ID","richmenu-4543fd08d262deae907195d6973593fb");
+define("BEARER_TOKEN","EDiLRqCWwuFXTmT2KGXddtlV2GVSg9kaTWJuJvsonJ1bbAKPCKISIyhavW4D5tL5tY7L+sU8jUkh+V7bxIP6lLTo7aXpV+QTKthC3vXAho+2nq50e2ZrzJguKtoC6Nhp4CLJajUtheyDbCyHvcHQ/gdB04t89/1O/w1cDnyilFU=");
 
 //Backup
 // define("REGISTER_URL", "http://rmxcell.pe.hu/rmxLineRegister.php");
