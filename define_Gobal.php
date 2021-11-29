5775ab06dc07cabf2694992c413506307a7ece0e
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-11-29 14:16:15 +0700

    1416

--- define_Gobal.php
+++ define_Gobal.php
@@ -1,32 +1,27 @@
 <?php
 
-$surl = "https://rmxliff.herokuapp.com/";
-
+//Heroku rmxdev Server
+define("COMPANY_CODE", "00001");
+// define("LIFF_ID","1656520973-EzB8pRze");
+// define("sURL","https://rmxdev.herokuapp.com/");
+// define("COMPANY_URL", "http://119.46.97.156/rmxline/rmxLineCmd.php");
+// define("REGISTER_URL", "http://119.46.97.156/rmxline/rmxLineRegister.php");
+// define("RICHMENU_ID","richmenu-133a752296d0fe1b5a51f275c1a770b3");
+// define("BEARER_TOKEN","EDiLRqCWwuFXTmT2KGXddtlV2GVSg9kaTWJuJvsonJ1bbAKPCKISIyhavW4D5tL5tY7L+sU8jUkh+V7bxIP6lLTo7aXpV+QTKthC3vXAho+2nq50e2ZrzJguKtoC6Nhp4CLJajUtheyDbCyHvcHQ/gdB04t89/1O/w1cDnyilFU=");
 
 //Heroku Server
-define("COMPANY_CODE", "00001");
-define("LIFF_ID", "1656503744-kojgw9pb");
-define("sURL", $surl);
-define("COMPANY_URL", $surl."rmxLineCmd.php");
-define("REGISTER_URL", $surl."rmxLineRegister.php");
+define("LIFF_ID", "1656520973-EzB8pRze");
+define("sURL", "https://rmxliff.herokuapp.com/");
+define("COMPANY_URL", "http://rmxliff.herokuapp.com/zApi/rmxLineCmd.php");
+define("REGISTER_URL", "http://rmxliff.herokuapp.com/zApi/rmxLineRegister.php");
 define("RICHMENU_ID","richmenu-c93df3fb38ad31cb5c52f127d08b5beb");
 define("BEARER_TOKEN","6DOzScAqBRwD/oRPwvMFua/SBvgLtXciCay4cwK10oTPA88R60mjeGdeW8NDL61dCJX2EtyHINFcj1DvY0mboZntH38a/fhTRI3rCaN4vDI/zWBCl0ze5K/AV2JoxoCwR9OZXj2Y7rHn6nABPwZMVwdB04t89/1O/w1cDnyilFU=");
 
-
-//RMX Server
-// define("COMPANY_CODE", "00001");
-// define("LIFF_ID","1656503744-kojgw9pb");
-// define("sURL","https://rmxliff.ddns.net/");
-// define("COMPANY_URL", "http://119.46.97.156/rmxline/rmxLineCmd.php");
-// define("REGISTER_URL", "http://119.46.97.156/rmxline/rmxLineRegister.php");
-// define("RICHMENU_ID","richmenu-58ef9edf18ba477f9ea2a0f12c28794c");
-// define("BEARER_TOKEN","6DOzScAqBRwD/oRPwvMFua/SBvgLtXciCay4cwK10oTPA88R60mjeGdeW8NDL61dCJX2EtyHINFcj1DvY0mboZntH38a/fhTRI3rCaN4vDI/zWBCl0ze5K/AV2JoxoCwR9OZXj2Y7rHn6nABPwZMVwdB04t89/1O/w1cDnyilFU=");
-
 //Heroku Server
 // define("LIFF_ID", "1656520973-EzB8pRze");
-// define("sURL", "https://rmxlineliff.herokuapp.com/");
-// define("COMPANY_URL", "http://rmxlineliff.herokuapp.com/zApi/rmxLineCmd.php");
-// define("REGISTER_URL", "http://rmxlineliff.herokuapp.com/zApi/rmxLineRegister.php");
+// define("sURL", "https://rmxliff.herokuapp.com/");
+// define("COMPANY_URL", "http://rmxliff.herokuapp.com/zApi/rmxLineCmd.php");
+// define("REGISTER_URL", "http://rmxliff.herokuapp.com/zApi/rmxLineRegister.php");
 // define("RICHMENU_ID","richmenu-4543fd08d262deae907195d6973593fb");
 // define("BEARER_TOKEN","EDiLRqCWwuFXTmT2KGXddtlV2GVSg9kaTWJuJvsonJ1bbAKPCKISIyhavW4D5tL5tY7L+sU8jUkh+V7bxIP6lLTo7aXpV+QTKthC3vXAho+2nq50e2ZrzJguKtoC6Nhp4CLJajUtheyDbCyHvcHQ/gdB04t89/1O/w1cDnyilFU=");
 
