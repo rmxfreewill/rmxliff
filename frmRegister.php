c89176d772a973d1db98bde5df1807a62d426287
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-12-01 22:05:01 +0700

    Update frmRegister.php

--- frmRegister.php
+++ frmRegister.php
@@ -420,7 +420,7 @@ if ($LineId != '' && $sFlagChangeMenu != false) {
             var sTel = document.getElementById('txtTel').value;
             // var sCmd = sLineDisplay + "^c" + sUserName + "^c" + sTel + "^c" + sEMail;
             // var para = "?LinkCode=REGISTER&LineId=" + sLineId + "&CmdCommand=" + sCmd;
-            alert(sTel);
+            alert('Hi');
         }
         function RegisterClick(msg) {
             var sLineId = document.getElementById('lblUserId').textContent;
