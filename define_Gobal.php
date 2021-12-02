ac0cb57e5fcb38aec7820bccb5886973026fcc5e
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2021-12-02 17:38:14 +0700

    Update define_Gobal.php

--- define_Gobal.php
+++ define_Gobal.php
@@ -14,7 +14,7 @@ define("LIFF_ID", "1656503744-kojgw9pb");
 define("sURL", "https://rmxliff.herokuapp.com/");
 // define("COMPANY_URL", "http://rmxliff.herokuapp.com/zApi/rmxLineCmd.php");
 // define("REGISTER_URL", "http://rmxliff.herokuapp.com/zApi/rmxLineRegister.php");
-define("RICHMENU_ID","richmenu-51aa8f8483635f43ed2b9198664e509f");
+define("RICHMENU_ID","richmenu-d8b3c212046b6876f19e33a2e9238f31");
 
 define("BEARER_TOKEN","6DOzScAqBRwD/oRPwvMFua/SBvgLtXciCay4cwK10oTPA88R60mjeGdeW8NDL61dCJX2EtyHINFcj1DvY0mboZntH38a/fhTRI3rCaN4vDI/zWBCl0ze5K/AV2JoxoCwR9OZXj2Y7rHn6nABPwZMVwdB04t89/1O/w1cDnyilFU=");
 
