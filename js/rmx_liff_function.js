1fc2798211eae36b092eeeda77e26a2eb9e5d03d
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-26 16:57:03 +0700

    func

--- js/rmx_liff_function.js
+++ js/rmx_liff_function.js
@@ -1,18 +1,13 @@
 async function initializeLiff(myLiffId,type) {
-
-    function typeMenu(type){
-        if(type=='LOGOUT'){
-            liff.isLoggedIn() ? liff.closeWindow() : alert('Thx');
-        }else if(type=='REGISTER'){
-            liff.isLoggedIn() ? getProfileLiffUserId() : liff.login();
-        }
-    }
-    
     await liff.init({
             liffId: myLiffId
         })
         .then(() => {
-            typeMenu(type);
+            if(type=='LOGOUT'){
+                liff.isLoggedIn() ? liff.closeWindow() : alert('Thx');
+            }else if(type=='REGISTER'){
+                liff.isLoggedIn() ? getProfileLiffUserId() : liff.login();
+            }
         })
         .catch((err) => {
             console.log("initializeLiff: " + err);
