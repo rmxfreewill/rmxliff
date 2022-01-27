d1a503828c8fa897db519d7378cfd34c95c27421
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-27 22:53:21 +0700

    2253

--- frmSearch.php
+++ frmSearch.php
@@ -87,7 +87,6 @@ function ticketSearchScreen($LineId)
     echo
     '<div class="login_container">
         <div class="login_container">
-
             <label for="txtFirst"><b>Start Date</b></label>
             <input type="date" dateformat="d M y" id="txtFirst">
             <label for="txtLast"><b>End Date</b></label>
@@ -97,6 +96,7 @@ function ticketSearchScreen($LineId)
             <input type="hidden" id="txtRet" value="<?php echo $RetCommand; ?>">
             <button type="button" id="btnSearch" onclick="SearchClick()">SEARCH</button>
         </div>
+        <button type="button" id="btnSearch" onclick="hi()">Hi</button>
     </div>';
 }
 
@@ -121,7 +121,7 @@ function ticketSearchScreen($LineId)
     <title><?php echo $sTitle; ?></title>
 
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/versions/2.3.0/sdk.js"></script>
-
+    <script charset="utf-8" src="js/rmx_liff_function.js"></script>
     <link rel="stylesheet" href="css/style.css">
 
     <style>
