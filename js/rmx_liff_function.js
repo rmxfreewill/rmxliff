e86db352c6855409cb6b00c2dee285d47a7f5fe2
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-28 21:23:02 +0700

    Update rmx_liff_function.js

--- js/rmx_liff_function.js
+++ js/rmx_liff_function.js
@@ -37,6 +37,9 @@ function rmxSelectMenu(toMenu, userId) {
         case "profile":
             url = URL + "frmProfile.php" + para;
             break;
+        case "hi":
+            url = URL + "hi.php";
+            break;
         default:
             url = URL + "frmLogout.php" + para;
             break;
