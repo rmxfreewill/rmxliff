5c3fbceb1641557ffb4e7682f5cd5f0263abc5df
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-28 21:21:58 +0700

    Create hi.php

---
+++ hi.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+error_reporting(E_ALL & ~E_NOTICE);
+include("rmxLineFunction.php");
+include("function/rmx_liff_function.php");
+
+header('Access-Control-Allow-Origin: *');
+
+$conn = mysqli_connect(HEROKU_HOST, HEROKU_USER, HEROKU_PASS, HEROKU_DB, PORT);
+if ($conn) {
+    echo "Connect";
+} else {
+    rmxhi();
+}
