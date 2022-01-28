ba45ac406830bdbfb79838b695f60a9765d4723d
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-28 15:17:07 +0700

    Update frmProfile.php

--- frmProfile.php
+++ frmProfile.php
@@ -6,21 +6,19 @@ header('Access-Control-Allow-Origin: *');
 
 error_reporting(E_ALL & ~E_NOTICE);
 include_once("rmxLineFunction.php");
-include_once("function/rmx_liff_function.php");
+// include_once("function/rmx_liff_function.php");
 
 $CompanyUrl = COMPANY_URL;
 $CompanyCode = COMPANY_CODE;
 $LiffId = LIFF_ID;
 $sURL = sURL;
-$menu='';
+$menu = '';
 if (isset($_POST['menu']))
     $menu = $_POST['menu'];
 if (isset($_GET['menu']))
     $menu = $_GET['menu'];
 
 ?>
-
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
@@ -32,23 +30,25 @@ if (isset($_GET['menu']))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="expires" content="0">
     <meta http-equiv="pragma" content="no-cache">
-    <title>Profile</title>
-
+    <link rel="stylesheet" href="css/style.css">
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/versions/2.18.1/sdk.js"></script>
     <script charset="utf-8" src="js/rmx_liff_function.js"></script>
+    <title>Profile</title>
 </head>
 
 <body>
-<?php
+    <?php
 
-getProfile();
+    echo $menu;
 
-rmxhi();
+    // getProfile();
 
-?>
-<script>
-    alert('Profile Page');
-</script>
+    // rmxhi();
+
+    ?>
+    <script>
+        alert('Profile Page');
+    </script>
 </body>
 
 </html>
