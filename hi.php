cab81e6ddb2a4b435a29fb52b1cd788d79555992
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-28 21:49:26 +0700

    Update hi.php

--- hi.php
+++ hi.php
@@ -4,8 +4,6 @@ session_start();
 
 // error_reporting(E_ALL & ~E_NOTICE);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include("rmxLineFunction.php");
