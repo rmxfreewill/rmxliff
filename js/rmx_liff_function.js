e36c87f00ac7329c7de5e41aafbdc28c837d3f3e
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-28 13:27:10 +0700

    Update rmx_liff_function.js

--- js/rmx_liff_function.js
+++ js/rmx_liff_function.js
@@ -24,7 +24,6 @@ function rmxSelectMenu(toMenu, userId) {
     var sCompCode = document.getElementById('txtCompanyCode').value;
     var sCmd = "call sp_main_check_register ('" + userId + "','" + sCompCode + "')";
     var para = "?LinkCode=CHECK&LineId=" + userId + "&CmdCommand=" + sCmd;
-    alert('toMenu: '+toMenu);
     switch (toMenu) {
         case "register":
             url = URL + "frmRegister.php" + para;
@@ -36,12 +35,12 @@ function rmxSelectMenu(toMenu, userId) {
             url = URL + "frmSearch.php" + para;
             break;
         case "profile":
-            // url = URL + "frmProfile.php" + para;
-            url = URL + "frmTest.php" + para;
+            url = URL + "frmProfile.php" + para;
+            // url = URL + "frmTest.php" + para;
             break;
-        default:
-            url = URL + "frmLogout.php" + para;
-            break;
+        // default:
+        //     url = URL + "frmLogout.php" + para;
+        //     break;
     }
 
     return url;
