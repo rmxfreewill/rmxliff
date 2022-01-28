3af3af5033bbe55823b12420d6658e8772c5a698
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-28 16:10:19 +0700

    Update rmx_liff_function.php

--- function/rmx_liff_function.php
+++ function/rmx_liff_function.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("rmxApi/define_Api_Global.php");
+include_once("../rmxApi/define_Api_Global.php");
 
 //Line Api
 function rmxChangeMemberRichMenuDefualt($LINEID)
@@ -37,7 +37,7 @@ function rmxChangeMemberRichMenuDefualt($LINEID)
 }
 
 function getProfile(){
-    $conn = mysqli_connect($HOST, $USER, $PASS, $DB, $PORT);
+    $conn = mysqli_connect(HOST, USER, PASS, DB, PORT);
 
     if ($conn) {
         print_r($conn);
