0623043e26a84a4a08b020d47e6a481be4d9b8eb
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-29 17:59:38 +0700

    Update rmxProfileLiff.php

--- rmxLiffFunction/rmxProfileLiff.php
+++ rmxLiffFunction/rmxProfileLiff.php
@@ -5,15 +5,15 @@ include(dirname(__FILE__) . "../../define_Global.php");
 function rmxGetProfileLiff($LineId)
 {
     $url = RMX_API_URL;
-    $data = "menu=profile&lineid=$LineId";
+    $param = "menu=profile&lineid=$LineId";
 
     $token = BEARER_TOKEN;
     $authorization = "Authorization: Bearer $token";
 
     try {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_URL, $url.$param);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
