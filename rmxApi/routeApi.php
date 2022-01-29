063f86d6bda9ba381119d814819537c6e3739742
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-29 15:05:21 +0700

    Update routeApi.php

--- rmxApi/routeApi.php
+++ rmxApi/routeApi.php
@@ -11,22 +11,20 @@ header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$bearer_token = BEARER_TOKEN;
-$header_token = trim($_SERVER['HTTP_AUTHORIZATION'], 'Bearer ');
-
+function getDataUrl()
+{
+    $get_string = $_SERVER['QUERY_STRING'];
+    parse_str($get_string, $get_array);
+    echo $get_array;
+}
 $requestMethod = $_SERVER["REQUEST_METHOD"] == 'POST';
 
+$bearer_token = BEARER_TOKEN;
+$header_token = trim($_SERVER['HTTP_AUTHORIZATION'], 'Bearer ');
+$isToken = $bearer_token == $header_token;
+echo $isToken;
 
-
-// echo $bearer_token . '==' . $header_token;
-
-// print_r( $header_token);
-
-
-// $parts = parse_url($_SERVER['QUERY_STRING']);
-
-$get_string = $_SERVER['QUERY_STRING'];
-
-parse_str($get_string, $get_array);
-
-print_r($get_array['menu']);
+if ($requestMethod) {
+    $a = getDataUrl();
+    echo $a;
+}
