78ae88c6168aa40017e9bd2bc291597055aba6c9
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-29 13:26:05 +0700

    Update routeApi.php

--- rmxApi/routeApi.php
+++ rmxApi/routeApi.php
@@ -8,17 +8,16 @@ header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode('/', $uri);
-
 $bearer_token = BEARER_TOKEN;
 $header_token = $_SERVER['HTTP_AUTHORIZATION']['Bearer'];
+
+//$_SERVER['QUERY_STRING']
 
 // echo $uri;
 
 // echo "<br>";
 
-echo $bearer_token == $header_token ? 'true' : 'false';
+echo $bearer_token . '==' . $header_token;
 
 // echo "<br>";
 
