db14eaafe1377983b58af9a2872f55c184a9451b
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-29 18:25:38 +0700

    Update rmxProfileLiff.php

--- rmxLiffFunction/rmxProfileLiff.php
+++ rmxLiffFunction/rmxProfileLiff.php
@@ -4,24 +4,21 @@ include(dirname(__FILE__) . "../../define_Global.php");
 
 function rmxGetProfileLiff($LineId)
 {
+    // $url = "https://jsonplaceholder.typicode.com/post/";
     $url = RMX_API_URL;
-    $url = "https://jsonplaceholder.typicode.com/post/";
     $param = "?menu=profile&lineid=$LineId";
 
+    $url = $url . $param;
     $headers = [
         "Authorization: Bearer " . BEARER_TOKEN
     ];
 
     try {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url . $param);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt(
-            $ch,
-            CURLOPT_POSTFIELDS,
-            array()
-        );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array());
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
         $data = curl_exec($ch);
@@ -34,4 +31,4 @@ function rmxGetProfileLiff($LineId)
     }
 }
 
-rmxGetProfileLiff('');
+rmxGetProfileLiff('Uc1dd5c7730988280c6c7731980655f7a');
