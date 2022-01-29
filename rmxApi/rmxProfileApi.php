5765385ef3e69913f88294d2df91b5c0a0a0e14d
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-29 18:30:09 +0700

    Update rmxProfileApi.php

--- rmxApi/rmxProfileApi.php
+++ rmxApi/rmxProfileApi.php
@@ -2,7 +2,12 @@
 
 include("define_rmxApi.php");
 
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 
 $GLOBALS['obj'] = new stdClass();
 
