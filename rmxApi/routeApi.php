aa54f34edf3241d1457d62122214fc2b71745f6f
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-29 12:46:09 +0700

    1246

--- rmxApi/routeApi.php
+++ rmxApi/routeApi.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hi';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+echo $uri;
+
+echo "<br>";
+
+echo $_POST["menu"];
+
+echo "<br>";
+
+print_r($_SERVER['HTTP_AUTHORIZATION']);
