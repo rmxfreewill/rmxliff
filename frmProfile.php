53d3457b127edec10ee68c49b3d8fb69457e5d7b
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-30 17:12:15 +0700

    Update frmProfile.php

--- frmProfile.php
+++ frmProfile.php
@@ -64,26 +64,26 @@ try {
                 <div class="card-body bg-gradient-success text-dark">
                     <div class="row">
                         <div class="col-5 text-uppercase text-bold">
-                            <h5>name:</h5>
+                            <h5>name</h5>
                         </div>
                         <div class="col-7">
-                            <h5>
+                          
                                 <?php
                                 echo $nameText;
                                 ?>
-                            </h5>
+                          
                         </div>
                     </div>
                     <div class="row">
                         <div class="card-text col-5 text-uppercase text-bold">
-                            <h5>mobile:</h5>
+                            <h5>mobile no</h5>
                         </div>
                         <div class="card-text col-7">
-                            <h5>
+                            
                                 <?php
                                 echo $mobileText;
                                 ?>
-                            </h5>
+                           
                         </div>
                     </div>
                 </div>
