b7a51e3dc1c084e3bb1b5840653e96e44a34d13c
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-01-31 18:13:25 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -190,18 +190,6 @@ $replyUserId = $jsonData["events"][0]["source"]["userId"];
 $MessageType = $jsonData["events"][0]["message"]["type"];
 $MessageText = $jsonData["events"][0]["message"]["text"];
 
-//
-// $textTypeParams = $jsonData["events"][0]["message"]["type"];
-// if ($textTypeParams == 'text') {
-//     $textParams = $jsonData["events"][0]["message"]["text"];
-//     if (strtolower($textParams) == 'status') {
-//         $replyJson["messages"][0] = ticketDetailFlexMessage();
-//     } else {
-//         $replyJson["messages"][0] = testFlexMessage($textParams);
-//     }
-// }
-//
-
 $replyJson["messages"][0] = replyJsonMessage($jsonData);
 $replyJson["to"] = $replyUserId;
 $replyJson["replyToken"] = $replyToken;
