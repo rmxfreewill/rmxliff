8226a9ffc2464787d301986be26f8d848c87fcc4
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-01 17:52:39 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -196,24 +196,7 @@ function replyJsonMessage($jsonData, $LineId)
             $data = testFlexMessage($tt);
         } else if ($case  == 'logout') {
             rmxChangeMemberRichMenuDefualt($LineId);
-?>
-            <html>
 
-            <head>
-                <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/versions/2.3.0/sdk.js"></script>
-                <script charset="utf-8" src="js/rmx_liff_function.js"></script>
-            </head>
-
-            <body>
-                <script>
-                    var myLiffId = document.getElementById('txtLiffId').value;
-                    var type = "logout";
-                    rmxInitializeLiff(myLiffId, type);
-                </script>
-            </body>
-
-            </html>
-<?php
         }
     }
     return $data;
