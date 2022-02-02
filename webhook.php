03a662e6c52f468bf63490829a6cd59313d56cc0
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 12:09:31 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -223,6 +223,7 @@ $personJSON = getLineIdAll($replyUserId);
 
 $person = json_decode($personJSON);
 
-print_r($person[0]);
+// print_r($person[0]);
 print_r($person['soldtocode']);
 print_r($person['lineid']);
+print_r($person->soldtocode);
