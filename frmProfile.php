e8b4511029baf7e6c23cbd1ff222695769de7ba9
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 15:23:41 +0700

    Update frmProfile.php

--- frmProfile.php
+++ frmProfile.php
@@ -129,6 +129,12 @@ function ticketSearchScreen($LineId,$arrRet)
     <script charset="utf-8" src="js/rmx_liff_function.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/search_style.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 
 <body>
