519171ec583eec224611a7d2eb33ad81b82f9085
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 16:11:09 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-// include("define_Global.php");
+include("define_Global.php");
 include("rmxLiffFunction.php");
 include("rmxLiffFunction/rmxProfileLiff.php");
 
@@ -12,8 +12,6 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-
 
 function sendMessage($replyJson)
 {
