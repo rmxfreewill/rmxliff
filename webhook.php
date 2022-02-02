840dec7ffe967642a235c4069448590af0f07cbe
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 17:36:24 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -92,9 +92,9 @@ function selectTicketDetail($LineId)
     $title = array("Ticket Number", "Product code", "Date", "Time", "Company Name", "Customer Name", "Contact Person", "Mobile", "Ship To Location", "Time to Load ", "Time to Leave", "Time to Jobsite", "Truck code", "Drive Name", "Load size (m3)", "Plant Code", "Product Name", "Slump", "Strength CU/CY", "Special Instruction");
     // $arrVal = array("1011808270007", "24/10/2018", "S01P901-00000331", "27/08/2018", "320000106 SH_Name 105", "997525133500 WPROOF PMP 25MPa 25mm S120 25@7DWPC1", "cV101 RMX Plant 101", "78", "2", "Theary Theary_", "FS22", "51E00491", "16:54:43", "Delivery", "5", "a", "a", "a", "a", "a");
     $arrVal = json_decode(rmxGetDataLiff('ticketdetails', $LineId), true);
-    echo $arrVal;
+    echo count($arrVal);
     for ($i = 0; $i < count($title); $i++) {
-        array_push($data, ticketDetailRowLayout($title[$i], $arrVal[0][$i]));
+        array_push($data, ticketDetailRowLayout($title[$i], $arrVal[$i]));
     }
 
     return $data;
