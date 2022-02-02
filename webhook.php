bda29421308ace7d8e2de65cfdd94181025db121
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 17:51:33 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -119,7 +119,7 @@ function ticketDetailFlexMessage($LineId)
     $objDetail->margin = "lg";
 
     $arrVal = json_decode(rmxGetDataLiff('ticketdetails', $LineId), true)[0];
-    echo $arrVal;
+    echo gettype($arrVal);
     $objDetail->contents = selectTicketDetail($arrVal);
 
     $output = array($objTitleH1, $objSeparator, $objDetail);
