4c084f89ca01d69fed2d82ab20be8f188908e49f
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 11:38:49 +0700

    1138

--- rmxLiffFunction/rmxProfileLiff.php
+++ rmxLiffFunction/rmxProfileLiff.php
@@ -2,6 +2,13 @@
 
 include("define_Global.php");
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
 function rmxGetProfileLiff($menu, $LineId)
 {
     // $rmx_api_url = RMX_HEROKU_API_URL;
--- webhook.php
+++ webhook.php
@@ -7,6 +7,13 @@ ini_set('display_errors', 'On');
 include("rmxLiffFunction.php");
 include("rmxLiffFunction/rmxProfileLiff.php");
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
 
 function sendMessage($replyJson)
 {
@@ -185,7 +192,7 @@ function getLineIdAll($LineId)
     $ProfileAndSoldtocode = rmxGetProfileLiff('ProfileAndSoldtocode', $LineId);
 
 
-    return json_encode($ProfileAndSoldtocode);
+    return $ProfileAndSoldtocode;
 }
 
 $LINEData = file_get_contents('php://input');
