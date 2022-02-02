a3234be1ed1d2c184a539ee78d2779dc46d272b0
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 09:37:23 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-include("define_Global.php");
+// include("define_Global.php");
 include("rmxLiffFunction.php");
 include("rmxLiffFunction/rmxProfileLiff.php");
 
