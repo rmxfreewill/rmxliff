757dbae152436f2570f9c6cbbe83ad82fc54ad4e
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 17:48:14 +0700

    1748

--- webhook.php
+++ webhook.php
@@ -119,7 +119,6 @@ function ticketDetailFlexMessage($LineId)
     $objDetail->margin = "lg";
 
     $arrVal = json_decode(rmxGetDataLiff('ticketdetails', $LineId), true)[0];
-    print_r($arrVal);
     $objDetail->contents = selectTicketDetail($arrVal);
 
     $output = array($objTitleH1, $objSeparator, $objDetail);
@@ -215,8 +214,8 @@ $replyJson["replyToken"] = $replyToken;
 $replyJson["to"] = getLineIdAll($replyUserId, 'lineid');
 $replyJson["messages"][0] = replyJsonMessage($jsonData, $replyUserId);
 $encodeJson = json_encode($replyJson);
-// $results = sendMessage($encodeJson);
-// echo $results;
+$results = sendMessage($encodeJson);
+echo $results;
 http_response_code(200);
 
 
