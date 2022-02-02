7d51fc1b1cd225e1523c3df668fd5cb986398633
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-02 15:15:07 +0700

    Update frmProfile.php

--- frmProfile.php
+++ frmProfile.php
@@ -2,26 +2,11 @@
 
 include_once("rmxLiffFunction/rmxProfileLiff.php");
 
-?>
 
-<html>
 
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="content-language" content="en-th">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="expires" content="0">
-    <meta http-equiv="pragma" content="no-cache">
-    <title><?php echo $sTitle; ?></title>
-    <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/versions/2.18.1/sdk.js"></script>
-    <script charset="utf-8" src="js/rmx_liff_function.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/search_style.css">
-</head>
-
+echo 
+"<html>
 <body>
-    FFFFFF
+Hu
 </body>
-
-</html>
+</html>";
