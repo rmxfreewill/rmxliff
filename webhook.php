dbd13cf3ea3f7aab986c9e78bc4c25bba726a805
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-03 10:05:49 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -99,10 +99,12 @@ function selectTicketDetail($arrVal)
 
     // $arrVal = array("1011808270007", "24/10/2018", "S01P901-00000331", "27/08/2018", "320000106 SH_Name 105", "997525133500 WPROOF PMP 25MPa 25mm S120 25@7DWPC1", "cV101 RMX Plant 101", "78", "2", "Theary Theary_", "FS22", "51E00491", "16:54:43", "Delivery", "5", "a", "a", "a", "a", "a");
 
-echo gettype($title);
-echo gettype($arrVal);
+
+
 
     for ($i = 0; $i < count($title); $i++) {
+        echo $title[$i]);
+        echo $arrVal[$i]);
         array_push($data, ticketDetailRowLayout($title[$i], $arrVal[$i]));
     }
 
