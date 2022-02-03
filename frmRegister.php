b9888fabb3d74d0a4133476e6efad88cf1212bb9
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-03 23:01:41 +0700

    Update frmRegister.php

--- frmRegister.php
+++ frmRegister.php
@@ -191,13 +191,11 @@ if ($LinkCode != 'LOGOUT') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="expires" content="0">
     <meta http-equiv="pragma" content="no-cache">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/versions/2.18.1/sdk.js"></script>
+
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/versions/2.18.1/sdk.js"></script>
+    <script charset="utf-8" src="rmx_liff_function.js"></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>RMX LINE OFFICIAL</title>
 </head>
 
