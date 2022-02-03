450726d17fbcaa298f94a0f6e5abe60878fa1a71
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-03 10:22:38 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -97,14 +97,15 @@ function selectTicketDetail($arrVal)
         "Slump", "Strength CU/CY", "Special Instruction"
     );
 
-    // $arrVal = array("1011808270007", "24/10/2018", "S01P901-00000331", "27/08/2018", "320000106 SH_Name 105", "997525133500 WPROOF PMP 25MPa 25mm S120 25@7DWPC1", "cV101 RMX Plant 101", "78", "2", "Theary Theary_", "FS22", "51E00491", "16:54:43", "Delivery", "5", "a", "a", "a", "a", "a");
+    $arrVal = array("1011808270007", "24/10/2018", "S01P901-00000331", "27/08/2018", "320000106 SH_Name 105", "997525133500 WPROOF PMP 25MPa 25mm S120 25@7DWPC1", "cV101 RMX Plant 101", "78", "2", "Theary Theary_", "FS22", "51E00491", "16:54:43", "Delivery", "5", "a", "a", "a", "a", "a","a");
 
 
 
 
     for ($i = 0; $i < count($title); $i++) {
-        echo $title[$i].' '.$arrVal[$i]."<br>";
+        // echo $title[$i].' '.$arrVal[$i]."<br/>";
         array_push($data, ticketDetailRowLayout($title[$i], $arrVal[$i]));
+        
     }
 
     return $data;
