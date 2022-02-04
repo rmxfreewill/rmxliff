709f2cf804de08d580681747754d35046fb9bb9b
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-04 16:42:02 +0700

    Update registerScreen.php

--- screen/registerScreen.php
+++ screen/registerScreen.php
@@ -3,7 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-include($_SERVER['DOCUMENT_ROOT'] . "/define_Global.php");
+// include($_SERVER['DOCUMENT_ROOT'] . "/define_Global.php");
 include("zScreenFunction.php");
 
 $CompanyUrl = COMPANY_URL;
