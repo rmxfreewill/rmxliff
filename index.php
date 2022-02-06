7053c839191b744b2d53940ab4a78320c4e4637a
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-06 17:54:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ $GLOBALS['sURL'] =   sURL;
                                     var menuUrl = null;
                                     if (toMenu == "register") {
                                         menuUrl = "menu/registerMenu.php";
-                                        $("#rmxLiFFLayout").load("registerMenu.php");
+                                        // $("#rmxLiFFLayout").load("registerMenu.php");
                                     } else if (toMenu == "ticket") {
 
                                     } else if (toMenu == "profile") {
@@ -90,7 +90,9 @@ $GLOBALS['sURL'] =   sURL;
 
                                     }
 
-                                    // menuUrl != null ?? $("#rmxLiFFLayout").load("registerMenu.php");
+                                    alert(menuUrl);
+
+                                    // menuUrl != null ?? $("#rmxLiFFLayout").load(menuUrl);
                                 }
                             })
                             .catch((err) => {
