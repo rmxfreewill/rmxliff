7e490d8ad112d0a678170b9181e24345f8819021
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-08 23:52:59 +0700

    Update searchMenu.php

--- menu/searchMenu.php
+++ menu/searchMenu.php
@@ -74,7 +74,7 @@ if ($status == 'init') {
 <?php
 if ($sFlag != '0') { //ticketSearchScreen($LineId);
 ?>
-    <div class="col-12 searchbox-color border border-white rounded rounded-lg p-3 mt-3">
+    <div class="col-12 searchbox-color border border-dark rounded rounded-lg p-3 mt-3">
         <div class="col-12 mb-3 text-center">
             <h3>Ticket Search</h3>
         </div>
