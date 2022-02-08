eb4c9d71891bac1aee3e7aaa4b637dc3b901d651
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-08 13:03:29 +0700

    1303

--- define_Global.php
+++ define_Global.php
@@ -12,6 +12,8 @@ define("sURL", "https://rmxliff.herokuapp.com/");
 
 //LINE Server
 //d8b3c212046b6876f19e33a2e9238f31
+//richmenu-ab649a7feedcfb978bc6681e5f7850b2 New
+//388f5b26e1355ae02b900d3729f7530c Old
 define("LIFF_ID", "1656503744-kojgw9pb");
-define("RICHMENU_ID","richmenu-388f5b26e1355ae02b900d3729f7530c");
+define("RICHMENU_ID","richmenu-ab649a7feedcfb978bc6681e5f7850b2");
 define("BEARER_TOKEN","6DOzScAqBRwD/oRPwvMFua/SBvgLtXciCay4cwK10oTPA88R60mjeGdeW8NDL61dCJX2EtyHINFcj1DvY0mboZntH38a/fhTRI3rCaN4vDI/zWBCl0ze5K/AV2JoxoCwR9OZXj2Y7rHn6nABPwZMVwdB04t89/1O/w1cDnyilFU=");
