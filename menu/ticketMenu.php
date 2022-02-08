91a33c49519edb38effdeadd094b0f1edb44bc0b
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-08 15:18:40 +0700

    Update ticketMenu.php

--- menu/ticketMenu.php
+++ menu/ticketMenu.php
@@ -44,14 +44,10 @@ if ($status == 'init') {
 
 <head>
     <title>RMX LINE OFFICIAL</title>
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="content-language" content="en-th">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-
 </head>
 
 <body>
