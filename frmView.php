6b9452654a9bdf301fb32ff3960606b37564a75c
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-09 11:50:33 +0700

    Update frmView.php

--- frmView.php
+++ frmView.php
@@ -3,7 +3,9 @@
 
   session_start();
 
-  error_reporting(E_ALL & ~E_NOTICE);
+  
+error_reporting(-1);
+ini_set('display_errors', 'On');
   include_once("rmxLineFunction.php");
   
   $CompanyUrl = COMPANY_URL;
