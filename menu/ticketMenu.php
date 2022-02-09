16bbb934123c08a390c4c198a64043a0e9cbfc40
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-09 10:35:14 +0700

    Update ticketMenu.php

--- menu/ticketMenu.php
+++ menu/ticketMenu.php
@@ -65,7 +65,9 @@ if ($status == 'init') {
                 if (count($arTmp) > 1) {
                     $asCol = explode("^c", $arTmp[0]);
                     $asRow = explode("^r", $arTmp[1]);
-echo json_encode($asRow);          
+echo json_encode($arTmp[0]); 
+echo"<br>Row<br>";
+echo json_encode($arTmp[1]);          
 // echo json_encode($asCol);
 // echo"<br>";
 // echo json_encode($asRow);
